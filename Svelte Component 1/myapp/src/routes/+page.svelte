8fd38ac6c68d55bea8d3a4dceceeff50745f1d88
eliza-kimball <156148008+eliza-kimball@users.noreply.github.com>
2024-09-19 19:41:03 -0600

    Svelte component 1

---
+++ Svelte Component 1/myapp/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import MyComponent from "./Component.svelte";
+</script>
+
+<svelte:head>
+<link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"/>
+</svelte:head>
+
+<MyComponent name="Button" type="regular" size="md"/>
---
+++ Svelte Component 1/myapp/src/routes/component.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let name = "Default";
+	export let type = "regular"; // regular, disabled
+	export let size = "md";
+	export let color = "indigo";
+	const sizes = {
+		"md" : "px-28 py-2.5",
+	}
+</script>
+
+{#if type=="regular"}
+ <button class={`rounded-full bg-${color}-600 text-white m-7 hover:bg-indigo-700 focus:ring focus:ring-indigo-400 ${(size in sizes) ? sizes[size] : sizes["md"]}`}>
+  {name}
+ </button>
+{/if}
+
+{#if type=="disabled"}
+ <button class={`rounded-full bg-gray-300 text-gray-500 m-7 ${(size in sizes) ? sizes[size] : sizes["md"]}`}>
+  {name}
+ </button> 
+{/if}
